b937140d3aa5635134cf6a8fc85d40176d2e1b32
AndresGonzalez5 <aagonzalez5@uc.cl>
2021-04-21 11:46:02 -0400

    bugfix template

--- Ayudantias/Ayudantia4/ejercicio_template.asm
+++ Ayudantias/Ayudantia4/ejercicio_template.asm
@@ -28,36 +28,4 @@
     Dreams: .word 4, 1, 2, 3, 5, 0
     Jobs: .word 6
 .text
-    start:
-        la s0, Current
-        la s1, Dreams
-        la a2, Jobs
-        lw a2, 0(a2)
-        mv t0, zero
-        addi a2, a2, -1
-
-    for:
-        lw a3, 0(s0)  # 0
-        lw a4, 0(s1)  # 4
-        bgt a3, a4, actual_es_mayor  # ifa3 >a41 arget
-        blt a3, a4, actual_es_menor # if a3 <a41 actual_es_menorarget
-        beq t0, a2, end
-        addi t0, t0, 1
-        addi s0, s0, 4
-        addi s1, s1, 4
-        j for
-        
-
-    actual_es_mayor:
-        addi a3, a3, -1
-        bgt a3, a4, actual_es_mayor
-        sb a3, 0(s0)
-        j for
-
-    actual_es_menor:
-        addi a3, a3, 1
-        blt a3, a4, actual_es_menor
-        sb a3, 0(s0)
-        j for
-
-    end:
+    start: 
